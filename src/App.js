ef5b81d699eef1163cb89241456ee0401ee7b351
Iden Han <idenhan129@gmail.com>
2021-02-06 01:43:47 +0900

    Implement Footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 
 import "./styles/main.sass";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <BrowserRouter>
         <Header />
         <Routes />
+        <Footer />
       </BrowserRouter>
     </>
   );
---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <section className="footer">
+      <div className="footer__content container">
+        <p>
+          Developed by
+          <a
+            href="https://www.idenhan.com"
+            rel="noreferrer"
+            target="_blank"
+            className="link"
+          >
+            Iden Han
+          </a>
+        </p>
+        <p>
+          Built with
+          <a href="https://reactjs.org/" className="link">
+            React JS
+          </a>
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Footer;
